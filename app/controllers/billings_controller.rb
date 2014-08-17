deb96cd64ec734cea947f4042e543c94150d2f0b
Sue Anne Teo <sueanneteo@gmail.com>
2014-08-17 20:57:16 +0000

    removed unsubscribe

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,21 +1,4 @@
 class BillingsController < ApplicationController
   before_filter :require_user
   
-  def unsubscribe
-    @user = current_user
-    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-    customer = Stripe::Customer.retrieve(@user.customer_token)
-    customer.subscriptions.retrieve(@user.payments.last.reference_id).delete #to retrieve the latest payment
-    @user.unsubscribe
-    # add an AppMailer
-    flash[:danger] = "Your MyFlix subscription has been cancelled. We hope you will come back soon."
-    redirect_to home_path
-  end
-  
-  private
-  
-  def find_user
-    @user = User.find(params[:id])
-  end
-  
 end
